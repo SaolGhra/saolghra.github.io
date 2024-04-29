d2b4179ca21d51a01e8e0207fed9a737c7d43324
SaolGhra <DeathCRystalYT@Gmail.com>
2024-04-29 22:01:54 +0100

    V3.5.2
    Updated my experiences and fixed the divider between it.

--- scripts/experiences.js
+++ scripts/experiences.js
@@ -2,13 +2,17 @@
 const experiences = [
   {
     date: "2018-2024",
-    title: "Head of Data Entry / Operations",
+    title: "Head of Data Entry / Operations - Volunteer",
+    description:
+      "Volunteer at Nexarda working in the Data Entry and Operations department. Responsible for managing the data entry team and ensuring the data is accurate and up-to-date. also responsible for managing the operations team and ensuring the smooth running of the operations department. Furthermore, i also worked on increasing the speed of games being added by coding a script that would automatically add games to the database.",
     company: "Nexarda",
   },
   {
     date: "2023 - 2024",
     title: "Student Placement",
-    company: "Placement for David Powell at Crawley College",
+    description:
+      "During my time on the placement, I worked on a variety of projects including crafting our own website called Atmosify which can be found in my projects section. I also worked on creating an information scraper that would scrape information from a website and store it in a database. this project can be found in my projects section under the name of 'Airliners Information Scraper'.",
+    company: "Placement with David Powell at Crawley College",
   },
 ];
 
@@ -23,7 +27,8 @@ function generateTimelineItems() {
           <div class="timeline-item-date">${exp.date}</div>
           <div class="timeline-item-details">
             <h3>${exp.title}</h3>
-            <p>${exp.company}</p>
+            <h4>${exp.company}</h4>
+            <h5>${exp.description}</h5>
           </div>
         </div>
       `;
